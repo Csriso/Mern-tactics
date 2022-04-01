601bf27e93a1c233dcd992a4ff9cb363a37c200e
unknown <cesyrub@gmail.com>
2022-04-01 18:50:28 +0200

    Controller and routes progress

--- backend/controllers/utilitiesController.js
+++ backend/controllers/utilitiesController.js
@@ -1,33 +1,39 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get Utilities
 // @route   GET /api/utilities
 // @access  Private
 
-const getUtilities = (req, res) => {
+const getUtilities = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get utilities" });
-};
+});
 
 // @desc    Set Utilities
 // @route   POST /api/utilities
 // @access  Private
 
-const setUtility = (req, res) => {
+const setUtility = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Add a text field");
+  }
   res.status(200).json({ message: "Set Utility" });
-};
+});
 
 // @desc    Update Utility
 // @route   PUT /api/utilities/:id
 // @access  Private
 
-const updateUtility = (req, res) => {
+const updateUtility = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update utility ${req.params.id}` });
-};
+});
 // @desc    Delete Utility
 // @route   DELETE /api/utilities/:id
 // @access  Private
 
-const deleteUtility = (req, res) => {
+const deleteUtility = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete utility ${req.params.id}` });
-};
+});
 
 module.exports = {
   getUtilities,
