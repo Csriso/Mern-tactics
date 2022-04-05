f300540af69dc8fc573460f7c797dc3381e87ff8
unknown <cesyrub@gmail.com>
2022-04-05 11:23:09 +0200

    DB connection

--- backend/controllers/utilitiesController.js
+++ backend/controllers/utilitiesController.js
@@ -1,11 +1,15 @@
 const asyncHandler = require("express-async-handler");
 
+const Utility = require("../models/utilityModel");
+
 // @desc    Get Utilities
 // @route   GET /api/utilities
 // @access  Private
 
 const getUtilities = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get utilities" });
+  const utilities = await Utility.find();
+
+  res.status(200).json(utilities);
 });
 
 // @desc    Set Utilities
@@ -17,7 +21,11 @@ const setUtility = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Add a text field");
   }
-  res.status(200).json({ message: "Set Utility" });
+
+  const utilities = await Utility.create({
+    text: req.body.text,
+  });
+  res.status(200).json(utilities);
 });
 
 // @desc    Update Utility
@@ -25,14 +33,36 @@ const setUtility = asyncHandler(async (req, res) => {
 // @access  Private
 
 const updateUtility = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update utility ${req.params.id}` });
+  const utility = await Utility.findById(req.params.id);
+
+  if (!utility) {
+    res.status(400);
+    throw new Error("Utility not found");
+  }
+
+  const updatedUtility = await Utility.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedUtility);
 });
+
 // @desc    Delete Utility
 // @route   DELETE /api/utilities/:id
 // @access  Private
 
 const deleteUtility = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete utility ${req.params.id}` });
+  const utility = await Utility.findById(req.params.id);
+
+  if (!utility) {
+    res.status(400);
+    throw new Error("Utility not found");
+  }
+  await utility.remove();
+
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
---
+++ backend/models/utilityModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const utilitySchema = mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: [true, "Please add a text value"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Utility", utilitySchema);
